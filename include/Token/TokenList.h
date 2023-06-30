46de400b476d6f91269e0282df8b597d0a3cbac7
itlantu <39409253+itlantu@users.noreply.github.com>
2023-06-30 21:46:06 +0800

    大改了Token::TokenList类

--- include/Token/TokenList.h
+++ include/Token/TokenList.h
@@ -3,6 +3,7 @@
 
 #include <cstdint>
 #include <array>
+#include <tuple>
 #include "Token/Token.h"
 
 using namespace std;
@@ -17,14 +18,23 @@ namespace toy::Token{
         ~TokenBlock();
 
         void setIndexValue(size_t index, const toy::Token::Token& value);
-        toy::Token::Token getIndexValue(size_t index);
+        toy::Token::Token getIndexValue(size_t index) const;
 
         void setIndexPosition(size_t index, array<size_t, 2> position_value);
-        array<size_t, 2> getIndexPosition(size_t index);
+        array<size_t, 2> getIndexPosition(size_t index) const;
     };
 
-    class TokenList: public TokenBlock{
+    class TokenList: private TokenBlock{
+    private:
+        int read_index = -1;
+        int write_index = -1;
+    public:
+        tuple<array<size_t, 2>, toy::Token::Token> operator ++();
 
+        void push(array<size_t, 2> position_value, toy::Token::Token value);
+        tuple<array<size_t, 2>, toy::Token::Token> pop();
+
+        bool isEnd() const;
     };
 }
 
--- src/Token/TokenList.cpp
+++ src/Token/TokenList.cpp
@@ -1,5 +1,6 @@
 #include "Token/TokenList.h"
 
+using namespace std;
 using namespace toy;
 
 Token::TokenBlock::TokenBlock() {
@@ -21,7 +22,7 @@ void Token::TokenBlock::setIndexValue(size_t index, const toy::Token::Token& val
     this->list[index] = value;
 }
 
-toy::Token::Token Token::TokenBlock::getIndexValue(size_t index) {
+toy::Token::Token Token::TokenBlock::getIndexValue(size_t index) const{
     if(index >= 32)
         return toy::Token::Token(toy::Token::TokenKind::None);
     return this->list[index];
@@ -32,8 +33,42 @@ void Token::TokenBlock::setIndexPosition(size_t index, array<size_t, 2> position
     this->position[index] = position_value;
 }
 
-array<size_t, 2> Token::TokenBlock::getIndexPosition(size_t index) {
+array<size_t, 2> Token::TokenBlock::getIndexPosition(size_t index) const{
     if(index >= 32)
         return array<size_t, 2>{0, 0};
     return this->position[index];
 }
+
+tuple<array<size_t, 2>, toy::Token::Token> Token::TokenList::operator++() {
+    return tuple<array<size_t, 2>, toy::Token::Token>();
+}
+
+void Token::TokenList::push(array<size_t, 2> position_value, toy::Token::Token value) {
+    if(this->write_index >= 32) return ;
+    auto& index = ++this->write_index;
+
+    this->setIndexValue(this->write_index, value);
+    this->setIndexPosition(this->write_index, position_value);
+}
+
+bool Token::TokenList::isEnd() const {
+    return this->read_index >= this->write_index;
+}
+
+tuple<array<size_t, 2>, toy::Token::Token> Token::TokenList::pop() {
+    auto ret = tuple<array<size_t, 2>, toy::Token::Token>();
+
+    if(this->read_index >= 32){
+        get<0>(ret) = array<size_t, 2>{0, 0};
+        get<1>(ret) = toy::Token::Token(toy::Token::TokenKind::None);
+    }else{
+        size_t index = this->read_index + 1;
+        get<0>(ret) = this->getIndexPosition(index);
+        get<1>(ret) = this->getIndexValue(index);
+    }
+
+    ++this->read_index;
+    return ret;
+}
+
+
