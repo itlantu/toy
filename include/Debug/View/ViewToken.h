c2746ce5e1f404c961cb78e38c8e43d527c9b3f0
itlantu <39409253+itlantu@users.noreply.github.com>
2023-06-30 22:19:39 +0800

    新增Debug::View::viewToken函数用于将Token格式化成字符串

--- include/Debug/View/ViewToken.h
+++ include/Debug/View/ViewToken.h
@@ -1,8 +1,16 @@
 #ifndef TOY_VIEWTOKEN_H
 #define TOY_VIEWTOKEN_H
 
+#include <string>
+#include "Token/Token.h"
+#include "Token/TokenList.h"
+
+using namespace std;
+
 namespace toy::Debug::View{
 
+    string viewToken(toy::Token::Token token, string_view source_filename="unknown");
+    string viewToken(tuple<array<size_t, 2>, toy::Token::Token> value, string_view source_filename="unknown");
 }
 
 #endif //TOY_VIEWTOKEN_H
--- src/Debug/View/ViewToken.cpp
+++ src/Debug/View/ViewToken.cpp
@@ -1,1 +1,37 @@
 #include "Debug/View/ViewToken.h"
+#include <string>
+#include <string_view>
+
+using namespace std;
+
+string toy::Debug::View::viewToken(tuple<array<size_t, 2>, toy::Token::Token> value, string_view source_filename) {
+    string ret;
+
+    auto &position =  get<0>(value);
+    auto token = get<1>(value);
+    auto token_value = token.to_string();
+    auto token_kind = token.getKind();
+
+    ret.append("Token <");
+
+    ret.append(source_filename);
+    ret.push_back(':');
+    ret.append(std::to_string(position[0]));
+    ret.push_back('.');
+    ret.append(std::to_string(position[1]));
+
+    ret.append("\t(");
+    ret.append(toy::Token::TokenKindTable[static_cast<short>(token_kind)]);
+    ret.append(" , ");
+
+    if(token_kind == toy::Token::TokenKind::String){
+        ret.push_back('"');
+        ret.append(token_value);
+        ret.push_back('"');
+    }else
+        ret.append(token_value);
+
+    ret.append(")>");
+
+    return ret;
+}
